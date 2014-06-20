c206fe4b04db3f19bdb7aa839df9ce968b0ecf69
Camille Avestruz of group nagai <ca347@login-0-0.local>
2014-06-20 11:22:20 -0400

    I am now putting my message in emacs, this is my first commit, and I've added generate_data.sh, which works.

---
+++ generate_data.sh
@@ -0,0 +1,7 @@
+# Use a for loop to create a text file called data1.txt with lines 1 through 10 (Hint:  You can append to the end of a file with >>)
+# Comment for this loop print 1 to 10
+
+for i in {1..10} ; 
+do
+    echo $i >> data1.txt
+done
